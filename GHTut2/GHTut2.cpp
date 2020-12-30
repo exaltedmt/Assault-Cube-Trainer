7030fbf559e131508d849aa987e95779c913fb3f
exaltedmt <rnh76959@uga.edu>
2020-12-30 14:49:13 -0500

    Add project files.

---
+++ GHTut2/GHTut2.cpp
@@ -0,0 +1,23 @@
+// HackAnyGamePt2.cpp : Defines the entry point for the console application
+//
+
+#include "stdafx.h"
+
+int main()
+{
+    //Get ProcID of the target process
+
+    //Getmodulebaseaddress
+
+    //Get Handle to Process
+
+    //Resolve base address of hte pointer chain
+
+    //Resolve our ammo pointer chain
+
+    //Read Ammo value
+
+    //Write to it
+
+    //Read out again
+}
